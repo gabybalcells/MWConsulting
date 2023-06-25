a183e89aeb8da0ffc2ceb3623490fad9ed1e94b6
Gaby Balcells <gkbalcells@gmail.com>
2023-06-24 18:55:32 -0600

    trying to fix rendering issues

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -26,19 +26,19 @@ export default function Header() {
         <AppBar style={{width: '100%'}}>
             <Container className='header' style={{width: '100%'}}>
                 <Toolbar className='navBar' style={{width: '100%'}}>
-                    {/* <Box> */}
-                        {/* <Stack > */}
-                            {/* <Box spacing={2}> */}
-                                {/* <div className='navBar'> */}
+                    <Box>
+                        <Stack >
+                            <Box spacing={2}>
+                                <div className='navBar'>
                                 <Typography sx={{p:2}}>Matt Wagner Consulting</Typography>
                                 <NavLink to='/services' className='navTab' sx={{m:2}}>Services</NavLink>
                                 <NavLink to='/industries' className='navTab' sx={{m:2}}>Industries</NavLink>
                                 <NavLink to='/industries' className='navTab' sx={{m:2}}>Insights</NavLink>
-                                {/* </div> */}
-                            {/* </Box> */}
+                                </div>
+                            </Box>
 
-                        {/* </Stack> */}
-                    {/* </Box> */}
+                        </Stack>
+                    </Box>
                        
                     {/* <Outlet /> */}
                 </Toolbar>
